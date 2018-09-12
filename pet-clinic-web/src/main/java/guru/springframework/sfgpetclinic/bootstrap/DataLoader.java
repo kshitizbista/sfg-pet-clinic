d5cd3427f70d5de4a670f97fedd9a3d949e9627c
kshitiz <tsetiz.bista@gmail.com>
2018-09-12 20:17:27 +0545

    fixed broken links. closes #45 created vet and speciality bootstrap. closes #34

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
-import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.model.Pet;
-import guru.springframework.sfgpetclinic.model.PetType;
-import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.model.*;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,16 +16,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        int count = petTypeService.findAll().size();
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogType = petTypeService.save(dog);
@@ -36,13 +43,24 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("Cat");
         PetType savedCatType = petTypeService.save(cat);
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Kshitiz");
         owner1.setLastName("Bista");
         owner1.setAddress(" Bhainsepati");
         owner1.setCity("Kathmandu");
         owner1.setTelephone("+911 67098");
-        ownerService.save(owner1);
 
         Pet kshitizPet = new Pet();
         kshitizPet.setPetType(savedDogType);
@@ -51,6 +69,7 @@ public class DataLoader implements CommandLineRunner {
         kshitizPet.setName("Rosco");
 
         owner1.getPets().add(kshitizPet);
+        ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Ram");
@@ -58,7 +77,6 @@ public class DataLoader implements CommandLineRunner {
         owner2.setAddress(" Bhainsepati");
         owner2.setCity("Kathmandu");
         owner2.setTelephone("+911 67098");
-        ownerService.save(owner2);
 
         Pet ramCat = new Pet();
         ramCat.setName("Just Cat");
@@ -67,20 +85,24 @@ public class DataLoader implements CommandLineRunner {
         ramCat.setOwner(owner2);
 
         owner2.getPets().add(ramCat);
+        ownerService.save(owner2);
 
-        System.out.println("Loaded Owners");
+        System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Turner");
+
+        vet1.getSpecialities().add(savedRadiology);
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+
+        vet2.getSpecialities().add(savedSurgery);
         vetService.save(vet2);
 
-        System.out.println("Loaded Vets");
-
+        System.out.println("Loaded Vets...");
     }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
     }
 }
